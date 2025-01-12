0b3f6fbe465ab718b17c142927877b84150dfaef
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-12 21:57:23 +0000

    Add prisma and initial db migration

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,19 @@
 import { serve } from '@hono/node-server';
+import { PrismaClient } from '@prisma/client';
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
 const app = new Hono();
 
-app.get('/', (ctx) => ctx.json({ result: 'Hello world' }));
+app.use(logger());
+
+app.get('/', async (ctx) => {
+  const db = new PrismaClient();
+  const count = await db.themePark.count();
+  const item = await db.themePark.findUnique({ where: { id: 1 } });
+  return ctx.json({ count, item });
+});
 
 serve(app, (info) => {
-  console.log(info);
   console.log(`Server started at http://localhost:${info.port}`);
 });
