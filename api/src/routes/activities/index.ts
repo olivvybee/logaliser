bc9b9a260b60a090ebb88ae100bfaa6ecaacb5d5
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-14 18:43:10 +0000

    Remove generic createActivity endpoint

--- api/src/routes/activities/index.ts
+++ api/src/routes/activities/index.ts
@@ -1,29 +1,7 @@
-import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 
-import { getDB } from '@/db';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { createActivity, createActivitySchema } from './createActivity';
 import { coasterActivityHandler } from './coaster';
 
 export const activitiesHandler = new Hono();
 
 activitiesHandler.route('/coaster', coasterActivityHandler);
-
-activitiesHandler.post(
-  '/',
-  zValidator('json', createActivitySchema),
-  async (ctx) => {
-    try {
-      const db = getDB();
-      const result = await createActivity(ctx.req.valid('json'), db);
-      return ctx.json(result);
-    } catch (err) {
-      const typedErr = err as PrismaClientKnownRequestError;
-      return ctx.json(
-        { error: { ...typedErr, message: typedErr.message } },
-        400
-      );
-    }
-  }
-);
