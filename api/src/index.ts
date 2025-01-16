e6c0dafddcf34a69deb18388c92526cc4c4786e1
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-16 17:16:52 +0000

    Allow port number to be set using env var

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,14 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
+import { config as loadEnv } from 'dotenv';
 
 import { themeParksHandler } from './routes/theme-parks';
 import { coastersHandler } from './routes/coasters';
 import { activitiesHandler } from './routes/activities';
+
+loadEnv();
+const PORT = Number(process.env.PORT) || 3000;
 
 const app = new Hono();
 
@@ -16,6 +20,12 @@ app.route('/activities', activitiesHandler);
 app.route('/coasters', coastersHandler);
 app.route('/theme-parks', themeParksHandler);
 
-serve(app, (info) => {
-  console.log(`Server started at http://localhost:${info.port}`);
-});
+serve(
+  {
+    ...app,
+    port: PORT,
+  },
+  (info) => {
+    console.log(`Server started at http://localhost:${info.port}`);
+  }
+);
