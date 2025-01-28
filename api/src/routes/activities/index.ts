5924b1ec8a9ef946c936309c44d7332a88a026b2
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-28 20:07:42 +0000

    Create endpoint for deleting an activity

--- api/src/routes/activities/index.ts
+++ api/src/routes/activities/index.ts
@@ -2,8 +2,34 @@ import { Hono } from 'hono';
 
 import { coasterActivityHandler } from './coaster';
 import { recentActivityHandler } from './recent';
+import { getDB } from '@/db';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
 export const activitiesHandler = new Hono();
 
 activitiesHandler.route('/coaster', coasterActivityHandler);
 activitiesHandler.route('/recent', recentActivityHandler);
+
+activitiesHandler.post(
+  '/delete/:id',
+  zValidator('param', z.object({ id: z.coerce.number() })),
+  async (ctx) => {
+    const id = parseInt(ctx.req.param('id'));
+    const db = getDB();
+
+    try {
+      await db.activity.delete({ where: { id } });
+    } catch (err) {
+      const typedError = err as PrismaClientKnownRequestError;
+      if (typedError.code === 'P2025') {
+        return ctx.notFound();
+      } else {
+        return ctx.json(typedError.message, 500);
+      }
+    }
+
+    return ctx.json({});
+  }
+);
