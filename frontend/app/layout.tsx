c841936d94c26ea40c563ec262a977bb962f7d14
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-28 22:40:14 +0000

    Set viewport properties

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 import './globals.css';
 import './theme.css';
@@ -7,16 +7,21 @@ import styles from './layout.module.css';
 
 import { Providers } from './Providers';
 import { TabBar } from '@/components/TabBar/TabBar';
+import { PropsWithChildren } from 'react';
 
 export const metadata: Metadata = {
   title: 'Logaliser',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  colorScheme: 'dark',
+};
+
+const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en">
       <body>
@@ -27,4 +32,6 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
