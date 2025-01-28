188252f31acea3556e9393fd41f30ba4b6c4b6f8
Liv Flowers <oliviaflowers93@gmail.com>
2025-01-28 22:36:50 +0000

    Adjust spacing of tabs

--- frontend/components/TabBar/TabBar.tsx
+++ frontend/components/TabBar/TabBar.tsx
@@ -8,7 +8,7 @@ export const TabBar = () => {
   return (
     <div className={styles.tabBar}>
       <div className={styles.tabs}>
-        <Link className={styles.tab} href="/">
+        <Link className={classNames(styles.tab, styles.leftTab)} href="/">
           <IconHistory />
           <span>Recent</span>
         </Link>
@@ -17,7 +17,7 @@ export const TabBar = () => {
           href="/logalise/coaster">
           <IconPlus size={48} stroke={3} />
         </Link>
-        <span className={styles.tab}>
+        <span className={classNames(styles.tab, styles.rightTab)}>
           <IconChartBar />
           <span>Stats</span>
         </span>
