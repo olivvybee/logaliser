db4a157233c1045b6843807b4b1a0eb2a53b26c9
Abby Flowers <oliviaflowers93@gmail.com>
2026-08-10 15:31:24 +0100

    Add entities and settings to tab bar

--- frontend/components/TabBar/TabBar.tsx
+++ frontend/components/TabBar/TabBar.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import classNames from 'classnames';
-import { IconPlus, IconHistory, IconChartBar } from '@tabler/icons-react';
+import {
+  IconPlus,
+  IconHistory,
+  IconChartBar,
+  IconSettings,
+  IconStack2,
+} from '@tabler/icons-react';
 
 import styles from './TabBar.module.css';
 
@@ -8,18 +14,28 @@ export const TabBar = () => {
   return (
     <div className={styles.tabBar}>
       <div className={styles.tabs}>
-        <Link className={classNames(styles.tab, styles.leftTab)} href="/">
+        <Link className={styles.tab} href="/">
           <IconHistory />
           <span>History</span>
+        </Link>
+        <Link className={classNames(styles.tab, styles.leftTab)} href="/stats">
+          <IconChartBar />
+          <span>Stats</span>
         </Link>
         <Link
           className={classNames(styles.tab, styles.logaliseButton)}
           href="/logalise">
           <IconPlus size={48} stroke={3} />
         </Link>
-        <Link className={classNames(styles.tab, styles.rightTab)} href="/stats">
-          <IconChartBar />
-          <span>Stats</span>
+        <Link
+          className={classNames(styles.tab, styles.rightTab)}
+          href="/entities">
+          <IconStack2 />
+          <span>Entities</span>
+        </Link>
+        <Link className={styles.tab} href="/settings">
+          <IconSettings />
+          <span>Settings</span>
         </Link>
       </div>
     </div>
